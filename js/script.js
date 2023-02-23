c5d918c877505bc798ca1f38d5e003f593e4bf87
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-02-23 16:31:40 +0100

    still building the form

--- js/script.js
+++ js/script.js
@@ -1,30 +1,36 @@
-/* Data
-0.21€/Km ticket's cost for km
--20% discount under 18
--40% discount  over 65
-? = ticket's cost normal and discount */
 
-let myDistance = prompt("How many kilometers do you have to travel?");
-let myAge = prompt("How old are you");
+// const myDistance = prompt("How many kilometers do you have to travel?");
+// const myAge = prompt("How old are you");
 
-myDistance = Number(myDistance);
-myAge = Number(myAge);
+// myDistance = Number(myDistance);
+// myAge = Number(myAge);
 
+// // Calculate the cost based on the distance and rate
+// const myCost = myDistance * 0.21;
+// myCost = Number(myCost);
 
-// Calculate the cost based on the distance and rate
-let myCost = myDistance * 0.21;
-myCost = Number(myCost);
+// if (myAge < 18) {
+//   myCost *= 0.8; // Apply a 20% discount for users under 18
+// } else if (myAge >= 65) {
+//   myCost *= 0.6; // Apply a 40% discount for users 65 and over
+// }
 
-if (myAge < 18) {
-    myCost *= 0.8; // Apply a 20% discount for users under 18
-  } else if (myAge >= 65) {
-    myCost *= 0.6; // Apply a 40% discount for users 65 and over
-}
+const generaBtn = document.querySelector('#generaBtn')
+const ressetBtn = document.querySelector('#resetBtn')
 
+generaBtn.addEventListener('click', 
+    function () {
+        const nameLastname = document.getElementById('nameLastname');
+        const msgNameLastname = nameLastname.value;
+        alert(msgNameLastname);
+    }
 
+);
 
-// Display the cost to the user
+resetBtn.addEventListener('click', 
+    function () {
+        const nameLastname = document.getElementById('nameLastname');
+        nameLastname.value  = '';
+    }
 
-console.log("The cost for the ticket is " + myCost.toFixed(2) + "€.");
-
-const userTicket = document.getElementById("ticket-info");
+);
